fced655c754f6c37ca002690a9b718580974bb65
mariogr93 <mariogr93@gmail.com>
2021-01-22 12:41:27 -0400

    Created users component with users hardcoded list to test

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-    return <h2>Users</h2>
+
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'mario',
+            image: 'https://img4.wikia.nocookie.net/__cb20090817174626/inciclopedia/images/4/49/Kenny_McCormick.png',
+            places: '3'
+        },
+        {
+            id: 'u2',
+            name: 'steph',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Kyle_Broflovski.svg/1200px-Kyle_Broflovski.svg.png',
+            places: '5'
+        },
+        {
+            id: 'u3',
+            name: 'ivan',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Eric_Cartman.svg/1200px-Eric_Cartman.svg.png',
+            places: '1'
+        },
+        {
+            id: 'u4',
+            name: 'jose',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Stan_Marsh.svg/1200px-Stan_Marsh.svg.png',
+            places: '6'
+        }
+    ]
+    return <UsersList items={USERS} />
+
 }
 
 export default Users
